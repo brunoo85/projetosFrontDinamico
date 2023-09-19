810a7429da6e8a82c411e51bcd82fd9a38f1bc1c
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-09-18 23:54:23 -0300

    primeiro commit

---
+++ 1 - calculadora/main.js
@@ -0,0 +1,94 @@
+// const result = document.getElementById("result");
+const operator = document.getElementById("operator");
+const numberValue = document.querySelectorAll(".keyNumber");
+const equal = document.getElementById("equal");
+let operadorEqual = false;
+
+
+numberValue.forEach((number) =>
+     number.addEventListener("click",() =>{
+          if(operadorEqual){
+               result.value="";
+               operator.innerHTML="";
+               operadorEqual=false;
+          }
+
+               result.value = result.value+number.innerHTML;
+
+          
+     })
+);
+
+let calcu = document.querySelectorAll(".operatior");
+
+let value1 =undefined;
+let value2= undefined;
+let value3=undefined;
+
+let operadorActive = false;
+
+
+calcu.forEach((calc) =>{
+     calc.addEventListener("click", ()=>{
+          if(operadorActive){
+          }else{
+               operator.innerHTML=calc.innerHTML;
+               value1 = parseFloat(result.value);
+               operadorActive=true;
+               result.value = "";
+          }
+          
+          if(!value1){
+               value1=parseFloat(result.value);
+          }
+     })
+})
+
+equal.addEventListener("click",() =>{
+     value2 = parseFloat(result.value);
+     if(value1&&value2){
+          
+          switch (operator.innerHTML) {
+               case "+":
+                    value3 = value1+value2;
+                    break;
+
+               case "-":
+                    value3 = value1-value2;
+                    break;
+
+               case "÷":
+                    value3 = value1/value2;
+                    break; 
+
+               case "x":
+                    value3 = value1*value2;
+                    break;
+               
+               default:
+                    console.log("deu algo errado");
+                    break;
+               
+          }
+          operator.innerHTML = "=";
+          result.value = value3;
+          operadorActive=false;
+          operadorEqual=true;
+     }
+})
+
+
+keyClean.addEventListener("click", () =>{
+     result.value ="";
+     if(operadorEqual){
+          operator.innerHTML="";
+     }
+})
+
+// document.addEventListener("keypress", (event) =>{
+//      console.log(event.key);
+//      // result.value = result.value+event.key
+//      if(operadorEqual){
+          
+//      }
+// })
