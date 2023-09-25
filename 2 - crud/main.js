8ff324d19413d3bc2d0c35a5ae8b331bdbf4d21e
Bruno Cesar <brunooufrn@gmail.com>
2023-09-25 13:38:05 -0300

    atualizações
    direção mais clara sobre oq fazer mais pra frente escrita no código main

--- 2 - crud/main.js
+++ 2 - crud/main.js
@@ -20,4 +20,37 @@ form.addEventListener("submit",(event)=>{
           imgPersonagem: imgPersonagem.value
         };
 
+        //colocar validaçoes
+        if(novoPersonagem.nameCaracter==""){
+
+        }
+
+
+        listaPersonagens.push(novoPersonagem); 
+        console.log(listaPersonagens); //passou, tá colocando na array 
+
+
+        //localStorage.setItem("arrayAnimais", JSON.stringify(arrayAnimais)); --> array do local storage do exemplo do professor
+        //limpa o form 
+        form.reset();
+})
+
+
+//cria o elemento no catalogo (teste)
+
+listaPersonagens.forEach((personagem, index)=>{
+     const catalogo = document.getElementById(catalogo);
+
+     const elem = document.createElement("div");
+     elem.className = "elem";
+
+     const img = document.createElement("img");
+     img.src=`images/serie${index}.jpeg`;
+     img.width="50";
+
+     const name = document.createElement("b");
+     name.innerText = personagem.nameCaracter;
+
+     elem.append(img,name);
+     catalogo.appendChild(elem);
 })
